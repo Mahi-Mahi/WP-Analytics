a46a859365e37e3298a5e4c94756741b02883336
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-01-15 18:36:53 +0100

    add config sample

---
+++ config_sample/config.php
@@ -0,0 +1,23 @@
+<?php
+
+// const WP_ANALYTICS_CLIENT_ID = '380700573108-ultlr1k746i390vguf8gpirsknpq4kp5.apps.googleusercontent.com';
+// const WP_ANALYTICS_SERVICE_ACCOUNT_NAME = '380700573108-cehlj9649g5d22sts22qt8c3mhfkprdl@developer.gserviceaccount.com';
+// const WP_ANALYTICS_KEY_FILE = '37ed36997c97819e019d8ef2e434e29cd4c4c031-privatekey.p12';
+
+$wp_analytics_config = array(
+
+	'client_id'				=>	'380700573108-ultlr1k746i390vguf8gpirsknpq4kp5.apps.googleusercontent.com',
+	'service_account_name'	=>	'380700573108-cehlj9649g5d22sts22qt8c3mhfkprdl@developer.gserviceaccount.com',
+
+	'ga_account'			=>	'ga:71980643',
+
+	'periods'				=>	array(
+		'hour'	=>	24,
+		'day'	=>	30,
+		'month'	=>	12,
+		'year'	=>	10
+	)
+
+);
+
+
